075724bea007bd4f1710ce3fbc93489fb17f7056
Hargun kaur <hargunkaur232@gmail.com>
2026-03-31 21:33:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let taskText = input.value.trim();
+
+    if (taskText === "") return;
+
+    let li = document.createElement("li");
+
+    let leftDiv = document.createElement("div");
+    leftDiv.className = "task-left";
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    let span = document.createElement("span");
+    span.innerText = taskText;
+
+    checkbox.onchange = function () {
+        span.classList.toggle("completed");
+    };
+
+    leftDiv.appendChild(checkbox);
+    leftDiv.appendChild(span);
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "✘ ";
+    deleteBtn.style.background = "transparent";
+    deleteBtn.style.color = "red";
+
+    deleteBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(leftDiv);
+    li.appendChild(deleteBtn);
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
